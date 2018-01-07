c5f7a35e22d484dd8bc176c71f8c3c9b1adba645
zengyy <zengyy26@mail2.sysu.edu.cn>
2018-01-07 22:31:48 +0800

    增加登录界面

--- FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wangjie.androidbucket.utils.ABTextUtil;
@@ -17,27 +21,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
     private RapidFloatingActionLayout rfaLayout;
     private RapidFloatingActionButton rfaBtn;
     private RapidFloatingActionHelper rfabHelper;
-//    private Button reg;
-//    private TextView username, password;
+    private Button reg, log;
+    private ImageView img;
+    private TextView username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bmob.initialize(this,"eef0e25fd7bc0a669af7fb37315b4a85");
         setContentView(R.layout.activity_main);
-        Intent intent1=new Intent(MainActivity.this,DiaryEditor.class);
-        startActivity(intent1);
 
         intent = new Intent(MainActivity.this, EditActivity.class);
 
         rfaLayout = (RapidFloatingActionLayout) findViewById(R.id.activity_main_rfal);
         rfaBtn = (RapidFloatingActionButton) findViewById(R.id.activity_main_rfab);
+
+        rfaBtn.setVisibility(View.INVISIBLE);
+        rfaLayout.setVisibility(View.INVISIBLE);
 
         RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(this);
         List<RFACLabelItem> items = new ArrayList<>();
@@ -107,31 +115,58 @@ public class MainActivity extends AppCompatActivity {
                }
             }
         });
-        // 测试数据库
-//        reg = (Button)findViewById(R.id.reg);
-//        username = (TextView)findViewById(R.id.username);
-//        password = (TextView)findViewById(R.id.password);
-//
-//        // 注册
-//        reg.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                User myuser = new User();
-//                myuser.setUsername(username.getText().toString());
-//                myuser.setPassword(password.getText().toString());
-//                myuser.signUp(new SaveListener<User>() {
-//                    @Override
-//                    public  void done(User user, BmobException e) {
-//                        if(e == null) {
-//                            toast("注册成功");
-//                        }else {
-//                            toast("注册失败");
-//                        }
-//                    }
-//                });
-//
-//            }
-//        });
+
+        reg = (Button) findViewById(R.id.reg);
+        log = (Button) findViewById(R.id.log);
+        username = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
+        img = (ImageView) findViewById(R.id.img);
+
+        // 注册
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User myuser = new User();
+                myuser.setUsername(username.getText().toString());
+                myuser.setPassword(password.getText().toString());
+                myuser.signUp(new SaveListener<User>() {
+                    @Override
+                    public  void done(User user, BmobException e) {
+                        if(e == null) {
+                            toast("注册成功");
+                        }else {
+                            toast("注册失败");
+                        }
+                    }
+                });
+            }
+        });
+
+        // 登录
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User myuser = new User();
+                myuser.setUsername(username.getText().toString());
+                myuser.setPassword(password.getText().toString());
+                myuser.login(new SaveListener<User>() {
+                    @Override
+                    public void done(User user, BmobException e) {
+                        if(e == null) {
+                            rfaBtn.setVisibility(View.VISIBLE);
+                            rfaLayout.setVisibility(View.VISIBLE);
+                            img.setVisibility(View.INVISIBLE);
+                            reg.setVisibility(View.INVISIBLE);
+                            log.setVisibility(View.INVISIBLE);
+                            username.setVisibility(View.INVISIBLE);
+                            password.setVisibility(View.INVISIBLE);
+                        }else {
+                            toast("登录失败");
+                        }
+                    }
+                });
+            }
+        });
 
     }
     public void toast(String string) {
