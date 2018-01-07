633f722d42be7250e254abeb0ed1ccf849da721f
hmengz <2313086554@qq.com>
2018-01-07 16:45:59 +0800

    富文本功能实现1

--- FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.yuying.finalproject;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent1=new Intent(MainActivity.this,DiaryEditor.class);
+        startActivity(intent1);
 
         intent = new Intent(MainActivity.this, EditActivity.class);
 
