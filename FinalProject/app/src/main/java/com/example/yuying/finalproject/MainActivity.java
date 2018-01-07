1e4011618a6e5c81d317da654957edcd05f673ce
zengyy <zengyy26@mail2.sysu.edu.cn>
2018-01-07 18:42:07 +0800

    增加浮动按钮

--- FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/yuying/finalproject/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.yuying.finalproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import com.wangjie.androidbucket.utils.ABTextUtil;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     private Intent intent;
+    private RapidFloatingActionLayout rfaLayout;
+    private RapidFloatingActionButton rfaBtn;
+    private RapidFloatingActionHelper rfabHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,75 @@ public class MainActivity extends AppCompatActivity {
 
         intent = new Intent(MainActivity.this, EditActivity.class);
 
-        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        rfaLayout = (RapidFloatingActionLayout) findViewById(R.id.activity_main_rfal);
+        rfaBtn = (RapidFloatingActionButton) findViewById(R.id.activity_main_rfab);
+
+        RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(this);
+        List<RFACLabelItem> items = new ArrayList<>();
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("新增")
+                .setResId(R.mipmap.add)
+                .setIconNormalColor(Color.WHITE)
+                .setIconPressedColor(0xffbf360c)
+                .setLabelColor(R.color.colorGrey)
+                .setWrapper(0)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("收藏夹")
+                .setResId(R.mipmap.mark)
+                .setIconNormalColor(Color.WHITE)
+                .setIconPressedColor(0xff3e2723)
+                .setLabelColor(R.color.colorGrey)
+                .setLabelSizeSp(14)
+              //  .setLabelBackgroundDrawable(ABShape.generateCornerShapeDrawable(0xaa000000, ABTextUtil.dip2px(this, 4)))
+                .setWrapper(1)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("社区")
+                .setResId(R.mipmap.community)
+                .setIconNormalColor(Color.WHITE)
+                .setIconPressedColor(0xff0d5302)
+                .setLabelColor(R.color.colorGrey)
+                .setWrapper(2)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("回收站")
+                .setResId(R.mipmap.delete)
+                .setIconNormalColor(Color.WHITE)
+                .setIconPressedColor(0xff1a237e)
+                .setLabelColor(R.color.colorGrey)
+                .setWrapper(3)
+        );
+        rfaContent
+                .setItems(items)
+                .setIconShadowRadius(ABTextUtil.dip2px(this, 5))
+                .setIconShadowColor(0xff888888)
+                .setIconShadowDy(ABTextUtil.dip2px(this, 5))
+        ;
+        rfabHelper = new RapidFloatingActionHelper(
+                this,
+                rfaLayout,
+                rfaBtn,
+                rfaContent
+        ).build();
+
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(new RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener() {
             @Override
-            public void onClick(View v){
-                startActivityForResult(intent, 0);
+            public void onRFACItemLabelClick(int position, RFACLabelItem item) {
+                /*Toast.makeText(MainActivity.this, "clicked label: " + position, Toast.LENGTH_SHORT).show();
+                rfabHelper.toggleContent();*/
+                if(position == 0){
+                    startActivityForResult(intent, 0);
+                }
+            }
+
+            @Override
+            public void onRFACItemIconClick(int position, RFACLabelItem item) {
+               /* Toast.makeText(MainActivity.this, "clicked icon: " + position, Toast.LENGTH_SHORT).show();
+                rfabHelper.toggleContent();*/
+               if(position == 0){
+                   startActivityForResult(intent, 0);
+               }
             }
         });
     }
