f211201d5597e1008bdd1c85b32afbfc33321fb8
zengyy <zengyy26@mail2.sysu.edu.cn>
2018-01-09 12:52:41 +0800

    UI界面修改

--- FinalProject/app/src/main/java/com/example/yuying/finalproject/HomeActivity.java
+++ FinalProject/app/src/main/java/com/example/yuying/finalproject/HomeActivity.java
@@ -177,7 +177,6 @@ public class HomeActivity extends AppCompatActivity {
                 } else if(position == 2){
                     Intent intent = new Intent(HomeActivity.this,Community.class);
                     startActivity(intent);
-                    startActivity(intent);
                 }
                 else if(position == 3){
                     Intent intent = new Intent(HomeActivity.this, DustbinActivity.class);
@@ -200,7 +199,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
                 else if(position == 3){
                     Intent intent = new Intent(HomeActivity.this, DustbinActivity.class);
-                    startActivity(intent);
                     startActivity(intent);
                 }
             }
